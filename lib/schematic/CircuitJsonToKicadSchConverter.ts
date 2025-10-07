a0383b67710dcbe522de866677ff2e8b27f89b5e
Rishabh Gupta <belikerishabh@gmail.com>
2025-10-07 20:34:34 +0530

    fix: use the A4 dimension for the the schematic output (#18)
    * fix: use the A4 dimension for the the schematic output
    
    * add comment

--- lib/schematic/CircuitJsonToKicadSchConverter.ts
+++ lib/schematic/CircuitJsonToKicadSchConverter.ts
@@ -24,8 +24,9 @@ export class CircuitJsonToKicadSchConverter {
 
   constructor(circuitJson: CircuitJson) {
     const CIRCUIT_JSON_SCALE_FACTOR = 15
-    const KICAD_CENTER_X = 95.25
-    const KICAD_CENTER_Y = 73.66
+    // A4 page dimensions
+    const KICAD_CENTER_Y = 105 // 210mm / 2
+    const KICAD_CENTER_X = 148.5 // 297mm / 2
 
     const db = cju(circuitJson)
 
