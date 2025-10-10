bc63906adf73ab9373cda02af2ceca89727df66e
imrishabh18 <belikerishabh@gmail.com>
2025-10-10 22:59:27 +0530

    support silkscreen which are independent and not part of the component

---
+++ tests/pcb/repros/ repro01-led-water-accelerometer-pcb.test.tsx
@@ -0,0 +1,31 @@
+import { test, expect } from "bun:test"
+import { CircuitJsonToKicadPcbConverter } from "lib/pcb/CircuitJsonToKicadPcbConverter"
+import { takeKicadSnapshot } from "../../fixtures/take-kicad-snapshot"
+import { takeCircuitJsonSnapshot } from "../../fixtures/take-circuit-json-snapshot"
+import { stackCircuitJsonKicadPngs } from "../../fixtures/stackCircuitJsonKicadPngs"
+import circuitJson from "tests/assets/led-water-accelerometer.json"
+
+test("pcb repro01", async () => {
+  const converter = new CircuitJsonToKicadPcbConverter(circuitJson as any)
+
+  converter.runUntilFinished()
+
+  Bun.write("./debug-output/kicad.kicad_pcb", converter.getOutputString())
+
+  const kicadSnapshot = await takeKicadSnapshot({
+    kicadFileContent: converter.getOutputString(),
+    kicadFileType: "pcb",
+  })
+
+  expect(kicadSnapshot.exitCode).toBe(0)
+
+  expect(
+    stackCircuitJsonKicadPngs(
+      await takeCircuitJsonSnapshot({
+        circuitJson: circuitJson as any,
+        outputType: "pcb",
+      }),
+      kicadSnapshot.generatedFileContent["temp_file.png"]!,
+    ),
+  ).toMatchPngSnapshot(import.meta.path)
+})
