f0c8a695db1328299c7a2331403023926490515f
techmannih <manishchaudharyttt@gmail.com>
2026-01-30 01:19:22 +0530

    Repro: max_resistance doesn't display for potentiometer

---
+++ tests/sch/repros/repro05-potentiometer-sch.test.tsx
@@ -0,0 +1,31 @@
+import { test, expect } from "bun:test"
+import { CircuitJsonToKicadSchConverter } from "lib/schematic/CircuitJsonToKicadSchConverter"
+import { takeKicadSnapshot } from "../../fixtures/take-kicad-snapshot"
+import { takeCircuitJsonSnapshot } from "../../fixtures/take-circuit-json-snapshot"
+import { stackCircuitJsonKicadPngs } from "../../fixtures/stackCircuitJsonKicadPngs"
+import circuitJson from "tests/assets/potentiometer.json"
+
+test("repro04 potentiometer symbol schematic", async () => {
+    const converter = new CircuitJsonToKicadSchConverter(circuitJson as any)
+
+    converter.runUntilFinished()
+
+    Bun.write("./debug-output/kicad.kicad_sch", converter.getOutputString())
+
+    const kicadSnapshot = await takeKicadSnapshot({
+        kicadFileContent: converter.getOutputString(),
+        kicadFileType: "sch",
+    })
+
+    expect(kicadSnapshot.exitCode).toBe(0)
+
+    expect(
+        stackCircuitJsonKicadPngs(
+            await takeCircuitJsonSnapshot({
+                circuitJson: circuitJson as any,
+                outputType: "schematic",
+            }),
+            kicadSnapshot.generatedFileContent["temp_file.png"]!,
+        ),
+    ).toMatchPngSnapshot(import.meta.path)
+})
