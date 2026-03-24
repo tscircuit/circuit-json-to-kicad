8f03d941a92cfd5bd6bf1818917d96b703179ecd
techmannih <manishchaudharyttt@gmail.com>
2026-03-24 07:12:05 +0530

    up

--- tests/pcb/repros/repro05-non-plated-hole-pcb.test.tsx
+++ tests/pcb/repros/repro05-non-plated-hole-pcb.test.tsx
@@ -1,11 +1,29 @@
 import { test, expect } from "bun:test"
+import { Circuit } from "tscircuit"
 import { CircuitJsonToKicadPcbConverter } from "lib/pcb/CircuitJsonToKicadPcbConverter"
 import { takeKicadSnapshot } from "../../fixtures/take-kicad-snapshot"
 import { takeCircuitJsonSnapshot } from "../../fixtures/take-circuit-json-snapshot"
 import { stackCircuitJsonKicadPngs } from "../../fixtures/stackCircuitJsonKicadPngs"
-import circuitJson from "tests/assets/non-plated-hole-circuit.json"
 
 test("pcb repro05 non-plated hole", async () => {
+  const circuit = new Circuit()
+  circuit.add(
+    <board width="50mm" height="50mm">
+      <hole pcbX={-20} pcbY={-20} diameter="3.2mm" />
+      <hole pcbX={20} pcbY={20} diameter="3.2mm" />
+      <resistor name="R1" resistance="10k" pcbX={0} pcbY={0} footprint="0402" />
+      <capacitor
+        name="C1"
+        capacitance="100nF"
+        pcbX={5}
+        pcbY={0}
+        footprint="0402"
+      />
+    </board>,
+  )
+  await circuit.renderUntilSettled()
+  const circuitJson = circuit.getCircuitJson()
+
   const converter = new CircuitJsonToKicadPcbConverter(circuitJson as any)
 
   converter.runUntilFinished()
@@ -14,6 +32,8 @@ test("pcb repro05 non-plated hole", async () => {
     "./debug-output/non-plated-hole.kicad_pcb",
     converter.getOutputString(),
   )
+
+  expect(converter.getOutputString()).toMatchSnapshot()
 
   const kicadSnapshot = await takeKicadSnapshot({
     kicadFileContent: converter.getOutputString(),
