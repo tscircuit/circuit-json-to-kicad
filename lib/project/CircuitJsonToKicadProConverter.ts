9a09563926b230a3c8cd5263d92af81e4b7e16a4
imrishabh18 <belikerishabh@gmail.com>
2026-04-20 15:24:45 -0700

    Add drc constraints for Drill size, track width and the via size

--- lib/project/CircuitJsonToKicadProConverter.ts
+++ lib/project/CircuitJsonToKicadProConverter.ts
@@ -161,7 +161,7 @@ export class CircuitJsonToKicadProConverter {
     const minViaDrill = pcbBoard?.min_via_hole_diameter ?? 0.2
     const minViaAnnularWidth = (minViaDiameter - minViaDrill) / 2
     const minTraceToPadSpacing = pcbBoard?.min_trace_to_pad_spacing ?? 0.1
-    const minTrackWidth = pcbBoard?.min_trace_width ?? 0.16
+    const minTrackWidth = pcbBoard?.min_trace_width ?? 0.1
     const netClasses: KicadProNetClass[] = [
       this.createBaseNetClass({
         name: "Default",
@@ -227,6 +227,9 @@ export class CircuitJsonToKicadProConverter {
           rules: {
             min_via_annular_width: minViaAnnularWidth,
             min_hole_clearance: minTraceToPadSpacing,
+            min_via_diameter: minViaDiameter,
+            min_track_width: minTrackWidth,
+            min_through_hole_diameter: minViaDrill, // Minimum Drill Size in kicad ui constraints
           },
         },
         last_opened_board: pcbFilename,
