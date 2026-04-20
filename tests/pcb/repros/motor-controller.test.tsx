dabec9f0aa1a0b5a2bfda00b25cf0672e78863f1
imrishabh18 <belikerishabh@gmail.com>
2026-04-19 17:11:21 -0700

    repro test for motor-controller

---
+++ tests/pcb/repros/motor-controller.test.tsx
@@ -0,0 +1,28 @@
+import { expect, test } from "bun:test"
+import { CircuitJsonToKicadPcbConverter } from "lib/pcb/CircuitJsonToKicadPcbConverter"
+import { CircuitJsonToKicadProConverter } from "lib/project/CircuitJsonToKicadProConverter"
+import circuitJson from "tests/assets/motor-controller.json"
+import { takeKicadSnapshot } from "../../fixtures/take-kicad-snapshot"
+
+test("motor controller pcb", async () => {
+  const proConverter = new CircuitJsonToKicadProConverter(circuitJson as any, {
+    projectName: "motor-controller",
+    schematicFilename: "motor-controller.kicad_sch",
+    pcbFilename: "motor-controller.kicad_pcb",
+  })
+  proConverter.runUntilFinished()
+
+  const converter = new CircuitJsonToKicadPcbConverter(circuitJson as any)
+
+  converter.runUntilFinished()
+
+  Bun.write("./debug-output/kicad.kicad_pro", proConverter.getOutputString())
+  Bun.write("./debug-output/kicad.kicad_pcb", converter.getOutputString())
+
+  const kicadSnapshot = await takeKicadSnapshot({
+    kicadFileContent: converter.getOutputString(),
+    kicadFileType: "pcb",
+  })
+
+  expect(kicadSnapshot.exitCode).toBe(0)
+})
