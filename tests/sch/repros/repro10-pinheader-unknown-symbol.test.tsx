c90aec3d58dcf82b37aabf6cfe0e66ec600feaad
mohan-bee <mohn08052006@gmail.com>
2026-04-29 10:45:23 +0530

    format

--- tests/sch/repros/repro10-pinheader-unknown-symbol.test.tsx
+++ tests/sch/repros/repro10-pinheader-unknown-symbol.test.tsx
@@ -10,85 +10,85 @@ test("repro10 pinheader schematic unknown symbol", async () => {
 
   circuit.add(
     <board width="30mm" height="30mm">
-  <pinheader
-    name="J2"
-    pinCount={8}
-    gender="male"
-    pitch="2.54mm"
-    footprint="pinrow8_rows2"
-    doubleRow={true}
-    showSilkscreenPinLabels={true}
-    pinLabels={["VCC", "GND", "SDA", "SCL", "MISO", "MOSI", "SCK", "CS"]}
-    pcbX={0}
-    pcbY={10}
-  />
-    <connector
-      name="J1"
-      manufacturerPartNumber="AF_QTZB1_0"
-      pinLabels={{
-        pin1: ["VCC"],
-        pin2: ["D_NEG"],
-        pin3: ["D_POS"],
-        pin4: ["GND"],
-        pin5: ["EH1"],
-        pin6: ["EH2"],
-      }}
-      footprint={
-        <footprint>
-          <hole pcbX="-2.5mm" pcbY="-2.125mm" diameter="1.3mm" />
-          <hole pcbX="2.5mm" pcbY="-2.125mm" diameter="1.3mm" />
-          <smtpad
-            portHints={["pin1"]}
-            pcbX="-3.5mm"
-            pcbY="1.575mm"
-            width="1.1mm"
-            height="3.8mm"
-            shape="rect"
-          />
-          <smtpad
-            portHints={["pin2"]}
-            pcbX="-1mm"
-            pcbY="1.575mm"
-            width="1.1mm"
-            height="3.8mm"
-            shape="rect"
-          />
-          <smtpad
-            portHints={["pin3"]}
-            pcbX="1mm"
-            pcbY="1.575mm"
-            width="1.1mm"
-            height="3.8mm"
-            shape="rect"
-          />
-          <smtpad
-            portHints={["pin4"]}
-            pcbX="3.5mm"
-            pcbY="1.575mm"
-            width="1.1mm"
-            height="3.8mm"
-            shape="rect"
-          />
-          <smtpad
-            portHints={["pin5"]}
-            pcbX="7.15mm"
-            pcbY="-1.475mm"
-            width="1.8mm"
-            height="4mm"
-            shape="rect"
-          />
-          <smtpad
-            portHints={["pin6"]}
-            pcbX="-7.15mm"
-            pcbY="-1.475mm"
-            width="1.8mm"
-            height="4mm"
-            shape="rect"
-          />
-        </footprint>
-      }
-    />
- </board>,
+      <pinheader
+        name="J2"
+        pinCount={8}
+        gender="male"
+        pitch="2.54mm"
+        footprint="pinrow8_rows2"
+        doubleRow={true}
+        showSilkscreenPinLabels={true}
+        pinLabels={["VCC", "GND", "SDA", "SCL", "MISO", "MOSI", "SCK", "CS"]}
+        pcbX={0}
+        pcbY={10}
+      />
+      <connector
+        name="J1"
+        manufacturerPartNumber="AF_QTZB1_0"
+        pinLabels={{
+          pin1: ["VCC"],
+          pin2: ["D_NEG"],
+          pin3: ["D_POS"],
+          pin4: ["GND"],
+          pin5: ["EH1"],
+          pin6: ["EH2"],
+        }}
+        footprint={
+          <footprint>
+            <hole pcbX="-2.5mm" pcbY="-2.125mm" diameter="1.3mm" />
+            <hole pcbX="2.5mm" pcbY="-2.125mm" diameter="1.3mm" />
+            <smtpad
+              portHints={["pin1"]}
+              pcbX="-3.5mm"
+              pcbY="1.575mm"
+              width="1.1mm"
+              height="3.8mm"
+              shape="rect"
+            />
+            <smtpad
+              portHints={["pin2"]}
+              pcbX="-1mm"
+              pcbY="1.575mm"
+              width="1.1mm"
+              height="3.8mm"
+              shape="rect"
+            />
+            <smtpad
+              portHints={["pin3"]}
+              pcbX="1mm"
+              pcbY="1.575mm"
+              width="1.1mm"
+              height="3.8mm"
+              shape="rect"
+            />
+            <smtpad
+              portHints={["pin4"]}
+              pcbX="3.5mm"
+              pcbY="1.575mm"
+              width="1.1mm"
+              height="3.8mm"
+              shape="rect"
+            />
+            <smtpad
+              portHints={["pin5"]}
+              pcbX="7.15mm"
+              pcbY="-1.475mm"
+              width="1.8mm"
+              height="4mm"
+              shape="rect"
+            />
+            <smtpad
+              portHints={["pin6"]}
+              pcbX="-7.15mm"
+              pcbY="-1.475mm"
+              width="1.8mm"
+              height="4mm"
+              shape="rect"
+            />
+          </footprint>
+        }
+      />
+    </board>,
   )
 
   await circuit.renderUntilSettled()
